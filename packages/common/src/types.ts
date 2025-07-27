b44567344f65803d0d6e1b4e67dc6a62a372253b
mayankbisht-tech <mayankbisht1107@gmail.com>
2025-07-27 21:02:38 +0530

    Fix: Correct start command and update Prisma versions

--- packages/common/src/types.ts
+++ packages/common/src/types.ts
@@ -4,14 +4,14 @@ export const CreateUserSchema = z.object({
     firstname: z.string().min(3).max(20),
     lastname: z.string().min(3).max(20),
     password: z.string(),
-    name: z.string()
+    email: z.email().min(3).max(20)
 })
 
 export const SigninSchema = z.object({
-    username: z.string().min(3).max(20),
+    email: z.email().min(3).max(20),
     password: z.string(),
 })
 
 export const CreateRoomSchema = z.object({
-    name: z.string().min(3).max(20),
+    email: z.email().min(3).max(20),
 })
